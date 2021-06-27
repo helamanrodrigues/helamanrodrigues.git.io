5e2ea23444a7cc2ba550634f776795d773dadb41
helamanrodrigues <u711602997@byui.edu>
2021-06-27 19:38:48 -0300

    update

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -5,3 +5,17 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+function openSection(event, sectionName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("sectionName");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" highlightTab", "");
+    }
+    document.getElementById(sectionName).style.display = "block";
+    event.currentTarget.className += " highlightTab";
+  }
